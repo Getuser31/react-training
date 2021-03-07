dbe2ebbfaf479e0db477ffc644d077257d40dbcf
Charles Fournet <charles.fournet@pm.me>
2021-03-07 15:50:22 +0100

    Break into small component Adding CSS

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Directoy from "../../components/directory/directory.component";
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directoy/>
+  </div>
+)
+
+export default HomePage;
