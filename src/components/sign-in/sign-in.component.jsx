6a40b67d04f398444c0dba01b9df8cdbd1b836e3
Charles Fournet <charles.fournet@pm.me>
2021-03-08 21:10:38 +0100

    Update Sign In component Create custom button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import './sign-in-styles.scss'
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
   constructor() {
@@ -35,7 +36,7 @@ class SignIn extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <FormInput name='email' type="email" value={this.state.email} handleChange={this.handleChange} label='email' required/>
           <FormInput name='password' type="password" value={this.state.password} handleChange={this.handleChange} label='password' required/>
-          <input type="submit" value='submit'/>
+          <CustomButton type="submit">  SIGN IN </CustomButton>
         </form>
       </div>
     )
