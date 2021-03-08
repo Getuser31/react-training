9c7dc2f7cf7217cc15a9b17652d021650c947e0c
Charles Fournet <charles.fournet@pm.me>
2021-03-08 20:03:23 +0100

    Updating shopPage with collection preview and item components

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -78,50 +78,50 @@ const SHOP_DATA =
       {
         id: 10,
         name: 'Adidas NMD',
-        // imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-        imageUrl: "/images/shop-img/sneakers/adidas-nmd.png",
+        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+        //imageUrl: "/images/shop-img/sneakers/adidas-nmd.png",
         price: 220
       },
       {
         id: 11,
         name: 'Adidas Yeezy',
-        // imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-        imageUrl: "/images/shop-img/sneakers/yeezy.png",
+        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+        //imageUrl: "/images/shop-img/sneakers/yeezy.png",
         price: 280
       },
       {
         id: 12,
         name: 'Black Converse',
-        // imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-        imageUrl: "/images/shop-img/sneakers/black-converse.png",
+        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
+        //imageUrl: "/images/shop-img/sneakers/black-converse.png",
         price: 110
       },
       {
         id: 13,
         name: 'Nike White AirForce',
-        // imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-        imageUrl: "/images/shop-img/sneakers/white-nike-high-tops.png",
+        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
+        //imageUrl: "/images/shop-img/sneakers/white-nike-high-tops.png",
         price: 160
       },
       {
         id: 14,
         name: 'Nike Red High Tops',
-        // imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-        imageUrl: "/images/shop-img/sneakers/nikes-red.png",
+        imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
+        //imageUrl: "/images/shop-img/sneakers/nikes-red.png",
         price: 160
       },
       {
         id: 15,
         name: 'Nike Brown High Tops',
-        // imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-        imageUrl: "/images/shop-img/sneakers/nike-brown.png",
+        imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
+        //imageUrl: "/images/shop-img/sneakers/nike-brown.png",
         price: 160
       },
       {
         id: 16,
         name: 'Air Jordan Limited',
-        // imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-        imageUrl: "/images/shop-img/sneakers/nike-funky.png",
+        imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
+        //imageUrl: "/images/shop-img/sneakers/nike-funky.png",
         price: 190
       },
       {
