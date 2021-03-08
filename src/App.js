682b8dbefb0c53f7ca43817d72e32efb19f67f10
Charles Fournet <charles.fournet@pm.me>
2021-03-08 20:16:21 +0100

    Crate header.component.jsx

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './pages/homepage/homepage.styles.scss';
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
       <HomePage/>
     </div>
