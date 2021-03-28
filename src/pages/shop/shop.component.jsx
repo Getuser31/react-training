1dbdc569bee3808601ca086d0337df89ac3fc154
Charles <charles.fournet@pm.me>
2021-03-28 12:14:17 +0200

    Move directory and shop data into reducer
    Create component with flow

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({collections}) => (
+const ShopPage = ({ match}) => (
   <div className='shop-page'>
-    {
-      <CollectionOverview/>
-    }
+   <Route path={`${match.path}`} component={CollectionOverview} />
+   <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 )
 
